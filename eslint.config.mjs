93b9ed75e069ac7cd633b110d8e3ab84f720ccce
SisselJM <144883413+SisselJM@users.noreply.github.com>
2024-04-18 11:25:03 +0200

    Add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,30 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin-js';
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin-js'
 
 export default [
-  {files: ['**/*.js'], languageOptions: {sourceType: 'commonjs'}},
-  {languageOptions: { globals: globals.browser }},
-  {plugins: {
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { ignores: ['dist'] },
+  { languageOptions: { globals: globals.browser } },
+  { plugins: {
     stylistic: stylistic
-  }},
+  } },
   pluginJs.configs.recommended,
   {
     rules: {
       'stylistic/indent': ['error', 2],
-      'stylistic/linebreak-style': ['error', 'unix'],
+      'stylistic/linebreak-style': ['warn', 'windows'],
       'stylistic/quotes': ['error', 'single'],
-      'stylistic/semi': ['error', 'never']
+      'stylistic/semi': ['error', 'never'],
+      'eqeqeq': 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': [
+        'error', 'always'
+      ],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
     }
   }
-];
+]
